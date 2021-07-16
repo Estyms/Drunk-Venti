aeb0b80c8654dde8fe33274e523f7f2f26369a2c
Evann Regnault <evann.regnault@pm.me>
2021-07-16 19:08:58 +0200

    Weekly fix

--- modules/dailyInfos.ts
+++ modules/dailyInfos.ts
@@ -28,7 +28,7 @@ function getDayName() {
         case 2: return "tuesday";
         case 3: return "wednesday";
         case 4: return "thursday";
-        case 5: return "wednesday";
+        case 5: return "friday";
         case 6: return "saturday";
         default:
             return ""
