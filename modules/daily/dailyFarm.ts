152a6ae83460d4cb5f48bd83512096f6647c0d6f
Evann Regnault <evann.regnault@pm.me>
2021-07-20 11:27:22 +0200

    Revert "Fixed Date errors"
    This reverts commit 402914fa8ebb984ea290e71e17d66d2b21663efd.

--- modules/daily/dailyFarm.ts
+++ modules/daily/dailyFarm.ts
@@ -1,5 +1,5 @@
 import { jsObjectFileToJson } from "../utils/jsObjectParser.ts"
-import { getGenshinDayName } from "../utils/timeRelated.ts"
+import { getDayName } from "../utils/timeRelated.ts"
 
 interface ItemDict {
     [index: string]: Item
@@ -17,6 +17,10 @@ interface FarmableItem extends Item {
     day: string[]
 }
 
+
+
+
+
 async function getItems() {
     const itemList: ItemDict = await jsObjectFileToJson("https://github.com/MadeBaruna/paimon-moe/raw/main/src/data/itemList.js") as unknown as ItemDict;
     return itemList;
@@ -27,7 +31,7 @@ async function getDailyFarmableItems(): Promise<FarmableItem[]> {
     const itemList = await getItems();
     const farmableItemKeys: string[] = Object.keys(itemList).filter((key: string) => itemList[key].day);
     const farmableItems: FarmableItem[] = farmableItemKeys.map(key => itemList[key]) as FarmableItem[];
-    const dailyFarmableItems: FarmableItem[] = farmableItems.filter(x => x.day.includes(getGenshinDayName()))
+    const dailyFarmableItems: FarmableItem[] = farmableItems.filter(x => x.day.includes(getDayName()))
     return dailyFarmableItems;
 }
 
